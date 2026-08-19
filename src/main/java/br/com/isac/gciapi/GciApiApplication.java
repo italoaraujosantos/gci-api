eeebe392d9cab0e78f1bc9906d288f04a3fb5bcb
Italo Araujo Chaves <italoaraujosantos@gmail.com>
2026-08-18 23:02:14 -0300

    Commit inicial.

---
+++ src/main/java/br/com/isac/gciapi/GciApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.isac.gciapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GciApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GciApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/isac/gciapi/GciApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isac.gciapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GciApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
